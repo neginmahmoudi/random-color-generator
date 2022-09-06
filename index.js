3c8dbc6933437f4ce6046d013474c44fb19c1801
Negin Mahmoudi <negin.mahmoudii76@gmail.com>
2022-09-06 18:11:27 +0200

    Update generator

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-const myNum = 24;
-console.log(myNum);
+import { argv } from 'node:process';
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const color = randomColor({
+  luminosity: argv[3] ? argv[3] : 'random',
+  hue: argv[2] ? argv[2] : 'random',
+});
+
+const theme = chalk.hex(color);
+
+const box = `
+##############################
+##############################
+##############################
+######                   #####
+######     ${color}       #####
+######                   #####
+##############################
+##############################
+##############################
+`;
+
+console.log(theme(box));
